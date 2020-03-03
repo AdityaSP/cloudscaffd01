4d5765a3f1bff62eae77bbf6af0e4e3a74c2ee41
SushmithaSharmaSushmitha <sushmitha.sharma@arithaconsulting.com>
2020-03-03 14:26:00 +0530

    Added alter script to database folder

---
+++ apache-ofbiz-16.11.05/database/db_alter_1.0.sql
@@ -0,0 +1,4 @@
+alter table base_pattern_apc modify BASE_FORCES LONGTEXT;
+alter table base_pattern_apc modify BASE_CONSEQUENCES LONGTEXT;
+alter table solution_design_apc modify SOLUTION_FORCES  LONGTEXT;
+alter table solution_design_apc modify SOLUTION_CONSEQUENCES LONGTEXT;
